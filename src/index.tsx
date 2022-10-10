399966bc921e36fe6f8cfead7ad0d4b701207763
tks18 <tksudharshan@gmail.com>
2022-10-10 21:13:22 +0530

    feat: render the new app component

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
+// Base
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './styles/index.scss';
+// Import Main App
+import { App } from '@app';
 
-// import '@fontsource/roboto/300.css';
-// import '@fontsource/roboto/400.css';
-// import '@fontsource/roboto/500.css';
-// import '@fontsource/roboto/700.css';
-
-import App from './app';
+// Import All the SCSS
+import '@styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <App />
