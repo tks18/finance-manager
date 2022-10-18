2143bc7df64a8e60101f7a268cb8f053c2086086
tks18 <tksudharshan@gmail.com>
2022-10-18 18:35:45 +0530

    feat(plugins): write a custom axios interceptor plugin

---
+++ src/plugins/axios/interceptor.ts
@@ -0,0 +1,28 @@
+import ax from 'axios';
+
+const instance = ax.create();
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  (config) => {
+    const conf = config;
+    conf.headers = config.headers || {};
+    conf.headers.requestStartedAt = String(new Date().getTime());
+    return conf;
+  },
+  (error) => Promise.reject(error),
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    const resp = response;
+    const conf = resp.config;
+    conf.headers = resp.config.headers || {};
+    conf.headers.requestEndedAt = String(new Date().getTime());
+    resp.config = conf;
+    return resp;
+  },
+  (error) => Promise.reject(error),
+);
+
+export const axios = instance;
