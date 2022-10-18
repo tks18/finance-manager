de707c5ceb13fde514056d3e61d4d784f7296a6a
tks18 <tksudharshan@gmail.com>
2022-10-18 18:51:57 +0530

    feat(plugins/store): write the persist config inside rootreducer file

--- src/plugins/store/reducers/index.ts
+++ src/plugins/store/reducers/index.ts
@@ -1,3 +1,4 @@
 export * from './root';
 export * from './theme';
 export * from './settings';
+export * from './user';
--- src/plugins/store/reducers/root.ts
+++ src/plugins/store/reducers/root.ts
@@ -1,8 +1,23 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 import { themeReducer } from './theme';
 import { settingsReducer } from './settings';
+import { userReducer } from './user';
 
-export const rootReducer = combineReducers({
+const combinedReducer = combineReducers({
   theme: themeReducer,
   settings: settingsReducer,
+  user: userReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['settings', 'theme', 'user'],
+};
+
+export const rootReducer = persistReducer<ReturnType<typeof combinedReducer>>(
+  persistConfig,
+  combinedReducer,
+);
