8245b9251119187a079db3ed92497e47a0f13e12
tks18 <tksudharshan@gmail.com>
2022-10-21 08:10:13 +0530

    refactor(routes): remove about and home from routes folder

--- src/routes/Home.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from '@mui/material';
-
-export function Home() {
-  return (
-    <Typography sx={{ color: 'primary.main', fontWeight: 'bold' }} variant="h1">
-      Home
-    </Typography>
-  );
-}
