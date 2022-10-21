5d15ff1de692b9c214157f7d306edada2f6cfd3c
tks18 <tksudharshan@gmail.com>
2022-10-21 07:43:36 +0530

    feat(plugins/store): add usertoken selector to user selectors

--- src/plugins/store/reducers/user/selector.ts
+++ src/plugins/store/reducers/user/selector.ts
@@ -1,6 +1,7 @@
 import { RootState } from '@plugins/store';
 
 export const userSelectors = {
+  userToken: (state: RootState) => state.user.token,
   userDoc: (state: RootState) => state.user.doc,
   loadingStates: (state: RootState) => state.user.loading,
   errorStates: (state: RootState) => state.user.errors,
