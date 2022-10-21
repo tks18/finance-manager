c3d422c7a9ee280acf23a382568d8c9b8590fc50
tks18 <tksudharshan@gmail.com>
2022-10-21 08:09:43 +0530

    refactor(routes/home): move the home to a seperate folder

---
+++ src/routes/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+
+export function Home() {
+  return (
+    <Typography sx={{ color: 'primary.main', fontWeight: 'bold' }} variant="h1">
+      Home
+    </Typography>
+  );
+}
