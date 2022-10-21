34d61f4b843a1f16960ddcac9844b3627c9e7d56
tks18 <tksudharshan@gmail.com>
2022-10-21 07:45:59 +0530

    refactor(plugins/backend/helpers): move requester to its own folder and maintain types inside that

---
+++ src/plugins/backend/helpers/requester/types.d.ts
@@ -0,0 +1,19 @@
+import type { AxiosRequestConfig } from 'axios';
+
+export interface IRequestOptions<T> {
+  url: string;
+  token?: string;
+  data?: T;
+  config?: AxiosRequestConfig<T>;
+}
+
+export interface IRequestResponse<T> {
+  status: number;
+  data: T;
+}
+
+export interface IRequestError {
+  status: number;
+  errorname: string;
+  message: string;
+}
