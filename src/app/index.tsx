04873f1e889ec3c17467c64843e2feba21589ac0
tks18 <tksudharshan@gmail.com>
2022-10-21 08:08:42 +0530

    refactor(app): properly setup app for react-router and move all contents to root routes file

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,26 +1,5 @@
-import {
-  StoreProvider,
-  ThemeProvider,
-  RouterProvider,
-  NavBar,
-  NavDrawer,
-} from '@components';
+import { RouterProvider } from '@components';
 
 export function App() {
-  const RouterPrependComponents = () => {
-    return (
-      <>
-        <NavBar />
-        <NavDrawer />
-      </>
-    );
-  };
-
-  return (
-    <StoreProvider>
-      <ThemeProvider>
-        <RouterProvider prependChildren={<RouterPrependComponents />} />
-      </ThemeProvider>
-    </StoreProvider>
-  );
+  return <RouterProvider />;
 }
