05c6998d9a26406bc6313383a810ce5a4fc884d4
tks18 <tksudharshan@gmail.com>
2022-10-23 20:08:26 +0530

    feat(routes/master): export everything from sub-folder and remove previous component

--- src/routes/templates/masters/index.tsx
+++ src/routes/templates/masters/index.tsx
@@ -1,9 +1,3 @@
-import { MainElement } from './main';
-import { ErrorElement } from './error';
-import { useBackendApi } from '@plugins/backend/hooks';
-
-export function MasterRoute() {
-  const apiConfig = useBackendApi();
-
-  return apiConfig !== undefined ? <MainElement /> : <ErrorElement />;
-}
+export * from './Root';
+export * from './Home';
+export * from './hooks';
