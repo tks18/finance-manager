9e518e2dd59484bdc956a4d33e6dfe3da5758044
tks18 <tksudharshan@gmail.com>
2022-10-25 07:51:53 +0530

    feat(routes/master): use the custom loader component

--- src/routes/templates/masters/Loader/index.tsx
+++ src/routes/templates/masters/Loader/index.tsx
@@ -1,17 +1,5 @@
-import { Skeleton, Box } from '@mui/material';
+import { Loader } from '@components';
 
-export function Loader() {
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-        height: '100%',
-      }}
-    >
-      <Skeleton variant="rounded" animation="wave" width={210} height={60} />
-    </Box>
-  );
+export function MasterLoader() {
+  return <Loader loaderText="Loading Master Table" />;
 }
