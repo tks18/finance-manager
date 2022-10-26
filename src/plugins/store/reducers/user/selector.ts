b4a8f889a2982f4f82822b11f06c40fd18d58760
tks18 <tksudharshan@gmail.com>
2022-10-26 10:33:27 +0530

    feat(plugins/store/user): create a selector for new prop

--- src/plugins/store/reducers/user/selector.ts
+++ src/plugins/store/reducers/user/selector.ts
@@ -1,6 +1,7 @@
 import { RootState } from '@plugins/store';
 
 export const userSelectors = {
+  userLoginType: (state: RootState) => state.user.loginType,
   userToken: (state: RootState) => state.user.token,
   userDoc: (state: RootState) => state.user.doc,
   loadingStates: (state: RootState) => state.user.loading,
