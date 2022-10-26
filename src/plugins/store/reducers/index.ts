36b6275c59d37fa52648f98564e097bb369e2451
tks18 <tksudharshan@gmail.com>
2022-10-26 12:51:27 +0530

    refactor(plugins/store): export session reducer and add it to root reducer

--- src/plugins/store/reducers/index.ts
+++ src/plugins/store/reducers/index.ts
@@ -2,3 +2,4 @@ export * from './root';
 export * from './theme';
 export * from './settings';
 export * from './user';
+export * from './session';
--- src/plugins/store/reducers/root.ts
+++ src/plugins/store/reducers/root.ts
@@ -4,17 +4,19 @@ import { persistReducer } from 'redux-persist';
 import { themeReducer } from './theme';
 import { settingsReducer } from './settings';
 import { userReducer } from './user';
+import { sessionReducer } from './session';
 
 const combinedReducer = combineReducers({
   theme: themeReducer,
   settings: settingsReducer,
   user: userReducer,
+  session: sessionReducer,
 });
 
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['settings', 'theme', 'user'],
+  blacklist: ['settings', 'theme', 'user', 'session'],
 };
 
 export const rootReducer = persistReducer<ReturnType<typeof combinedReducer>>(
