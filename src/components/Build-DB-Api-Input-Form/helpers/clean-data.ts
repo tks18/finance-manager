5f7a7a9540d8e5c7c007b97c84f643a8fc7b00cf
tks18 <tksudharshan@gmail.com>
2023-08-15 22:33:39 +0530

    refactor: update types and refactor

--- src/components/Build-DB-Api-Input-Form/helpers/clean-data.ts
+++ src/components/Build-DB-Api-Input-Form/helpers/clean-data.ts
@@ -1,9 +1,13 @@
-export const cleanData = (state: { [key: string]: any }) => {
+import { TGenericCreationAttributes } from '@plugins/backend/types';
+
+export const cleanData = <StateType extends Record<string, any>>(
+  state: StateType,
+): TGenericCreationAttributes => {
   const cleanedData: { [key: string]: any } = {};
   for (const [obj, vals] of Object.entries(state)) {
     if (vals !== '') {
       cleanedData[obj] = vals;
     }
   }
-  return cleanedData;
+  return cleanedData as TGenericCreationAttributes;
 };
